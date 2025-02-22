9310b8c36c24c8693f2408c2b622c83f164a1d16
Heryan Djaruma <heryandjaruma@gmail.com>
2025-02-23 00:08:54 +0700

    proper keyboard layout

--- Components/GrainedComponents/Keyboard.swift
+++ Components/GrainedComponents/Keyboard.swift
@@ -8,37 +8,46 @@
 import SwiftUI
 
 struct Keyboard: View {
+    private let screenWidth = UIScreen.main.bounds.width
     private let keyWidth = UIScreen.main.bounds.width / 15 // Width of each white key
+    private let screenHeight = UIScreen.main.bounds.height
     
     var body: some View {
-        ZStack {
-            // White Keys Layer
+        ZStack(alignment: .top) {
             HStack(spacing: 0) {
                 ForEach(whiteKeys) { key in
                     WhiteKey(pianoKey: key)
-                        .frame(width: keyWidth) // Set consistent width
+                        .frame(width: keyWidth)
                 }
             }
+            .position(x: screenWidth / 2 , y: screenHeight * 4 / 5)
+            .offset(x: 0, y: screenHeight / 5 / 2)
             
-            // Black Keys Layer (Stacked Above, Using Absolute Positioning)
-            HStack(spacing: 0) {
+            ZStack {
                 ForEach(blackKeys) { key in
-                    if let position = blackKeyPosition[key.name] {
+                    if let posMultiplier = blackKeysMultipliers[key.name] {
                         BlackKey(pianoKey: key)
-                            .frame(width: 3) // Make black keys smaller
-                            .offset(x: (position - 7) * keyWidth) // Center over white keys
+                            .position(x: keyWidth * posMultiplier, y: screenHeight * 4 / 5)
+                            .offset(x: 0, y: screenHeight / 5 / 2 * 0.65)
                     }
                 }
             }
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
-let blackKeyPosition: [String: CGFloat] = [
-    "C#4": 0.75, "D#4": 1.75,
-    "F#4": 3.75, "G#4": 4.75, "A#4": 5.75,
-    "C#5": 7.75, "D#5": 8.75,
-    "F#5": 10.75, "G#5": 11.75, "A#5": 12.75
+let blackKeysMultipliers: [String: CGFloat] = [
+    "C#4": 1,
+    "D#4": 2,
+    "F#4": 4,
+    "G#4": 5,
+    "A#4": 6,
+    "C#5": 8,
+    "D#5": 9,
+    "F#5": 11,
+    "G#5": 12,
+    "A#5": 13
 ]
 
 #Preview {
