5264c64205a24a530510ae3f4543a2b6d1125132
Heryan Djaruma <heryandjaruma@gmail.com>
2025-02-23 23:42:43 +0700

    add sample manager, add scenes

---
+++ Views/Scenes/StartPromptView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Perfect Pitch
+//
+//  Created by Heryan Djaruma on 23/02/25.
+//
+
+import SwiftUI
+
+struct StartPromptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartPromptView()
+}
---
+++ Views/Scenes/TwelveNotesTheoryView.swift
@@ -0,0 +1,18 @@
+//
+//  TwelveNotesTheory.swift
+//  Perfect Pitch
+//
+//  Created by Heryan Djaruma on 23/02/25.
+//
+
+import SwiftUI
+
+struct TwelveNotesTheoryView: View {
+    var body: some View {
+        Text("Tweleve Notes View")
+    }
+}
+
+#Preview {
+    TwelveNotesTheoryView()
+}
